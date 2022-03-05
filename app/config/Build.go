bb1792d97009c563799fb9e4d1c404c0e88dbd4a
Juan Diego Melo <jdmelo002@gmail.com>
2022-03-05 11:17:15 -0500

    Fix cors

--- app/config/Build.go
+++ app/config/Build.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"accounts-service/app/controllers"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,12 +10,6 @@ func DefinePaths(router *gin.RouterGroup) {
 	router.POST("/user", controllers.CreateNewUserController)
 
 	router.GET("/role", controllers.ConsultRolesController)
-}
-
-func SetCors(router *gin.RouterGroup) {
-	router.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"*"},
-	}))
 }
 
 func SetLogger(router *gin.RouterGroup) {
